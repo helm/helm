e1cb0ede863e056082d2b9c82a9310b15ea1613e
vaikas-google <vaikas@google.com>
2015-12-09 11:18:17 -0800

    handle fetching number of urls as a unit that get pooled into a single template so that helm works out of the box

--- manager/manager/expander.go
+++ manager/manager/expander.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/ghodss/yaml"
@@ -140,6 +141,8 @@ func (e *expander) ExpandTemplate(t *common.Template) (*ExpandedTemplate, error)
 
 	t.Imports = append(t.Imports, newImp...)
 
+	log.Printf("Got Imports as %#v", t.Imports)
+
 	for {
 		// Now expand with everything imported.
 		result, err := e.expandTemplate(t)
@@ -166,6 +169,7 @@ func (e *expander) ExpandTemplate(t *common.Template) (*ExpandedTemplate, error)
 		// If the new imports contain nothing, we are done. Everything is fully expanded.
 		if len(newImp) == 0 {
 			result.Layout = finalLayout
+			log.Printf("Returning config as %#v", result)
 			return result, nil
 		}
 
