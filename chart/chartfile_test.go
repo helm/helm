15218cb02b696defd77f58d216316456df945f28
Matt Butcher <mbutcher@engineyard.com>
2016-01-27 18:01:34 -0700

    Add Chart tar reader, dir reader, tests.

--- chart/chartfile_test.go
+++ chart/chartfile_test.go
@@ -1,60 +1,58 @@
 package chart
 
 import (
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
-var testChart = `#helm:generate foo
-name: frobniz
-description: This is a frobniz.
-version: 1.2.3-alpha.1+12345
-keywords:
-	- frobnitz
-	- sprocket
-	- dodad
-maintainers:
-	- name: The Helm Team
-	  email: helm@example.com
-	- name: Someone Else
-	  email: nobody@example.com
-source: https://example.com/foo/bar
-home: http://example.com
-dependencies:
-	- name: thingerbob
-	  location: https://example.com/charts/thingerbob-3.2.1.tgz
-	  version: ^3
-environment:
-	- name: Kubernetes
-	  version: ~1.1
-	  extensions:
-	  	- extensions/v1beta1
-	  	- extensions/v1beta1/daemonset
-	  apiGroups:
-	  	- 3rdParty
-`
-
 func TestLoadChartfile(t *testing.T) {
-	out, err := ioutil.TempFile("", "chartfile-")
+	f, err := LoadChartfile(testfile)
 	if err != nil {
-		t.Fatal(err)
-	}
-	tname := out.Name()
-	defer func() {
-		os.Remove(tname)
-	}()
-
-	out.Write([]byte(testChart))
-	out.Close()
-
-	c, err := LoadChartfile(tname)
-	if err != nil {
-		t.Errorf("Failed to open %s: %s", tname, err)
+		t.Errorf("Failed to open %s: %s", testfile, err)
 		return
 	}
 
-	if len(c.Environment[0].Extensions) != 2 {
-		t.Errorf("Expected two extensions, got %d", len(c.Environment[0].Extensions))
+	if len(f.Environment[0].Extensions) != 2 {
+		t.Errorf("Expected two extensions, got %d", len(f.Environment[0].Extensions))
 	}
+
+	if f.Name != "frobnitz" {
+		t.Errorf("Expected frobnitz, got %s", f.Name)
+	}
+
+	if len(f.Maintainers) != 2 {
+		t.Errorf("Expected 2 maintainers, got %d", len(f.Maintainers))
+	}
+
+	if len(f.Dependencies) != 1 {
+		t.Errorf("Expected 2 dependencies, got %d", len(f.Dependencies))
+	}
+
+	if f.Dependencies[0].Name != "thingerbob" {
+		t.Errorf("Expected second dependency to be thingerbob: %q", f.Dependencies[0].Name)
+	}
+
+	if f.Source[0] != "https://example.com/helm" {
+		t.Errorf("Expected https://example.com/helm, got %s", f.Source)
+	}
+}
+
+func TestVersionOK(t *testing.T) {
+	f, err := LoadChartfile(testfile)
+	if err != nil {
+		t.Errorf("Error loading %s: %s", testfile, err)
+	}
+
+	// These are canaries. The SemVer package exhuastively tests the
+	// various  permutations. This will alert us if we wired it up
+	// incorrectly.
+
+	d := f.Dependencies[0]
+	if d.VersionOK("1.0.0") {
+		t.Errorf("1.0.0 should have been marked out of range")
+	}
+
+	if !d.VersionOK("3.2.3") {
+		t.Errorf("Version 3.2.3 should have been marked in-range")
+	}
+
 }
---
+++ chart/doc.go
@@ -0,0 +1,6 @@
+/* Package chart implements the Chart format.
+
+This package provides tools for working with the Chart format, including the
+Chartfile (chart.yaml) and compressed chart archives.
+*/
+package chart
