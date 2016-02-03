1e37113c398efeeaa0ee89f6a743950597272649
Adam Reese <adam@reese.io>
2016-02-03 12:04:42 -0800

    feat(dm-delete): add dm-delete command

---
+++ dm/uninstall.go
@@ -0,0 +1,14 @@
+package dm
+
+import (
+	"github.com/deis/helm-dm/kubectl"
+)
+
+// uninstall uses kubectl to uninstall the base DM.
+//
+// Returns the string output received from the operation, and an error if the
+// command failed.
+func Uninstall(runner kubectl.Runner) (string, error) {
+	o, err := runner.Delete("dm", "Namespace", "dm")
+	return string(o), err
+}
