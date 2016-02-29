b916b2b102f981e46cf2dc78750ad17ba8577f4e
Adam Reese <adam@reese.io>
2016-02-29 10:24:15 -0800

    fix(version.go): make version updatable by build scripts

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,7 +6,7 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
- 
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,4 +22,4 @@ package version
 // Increment major number for new feature additions and behavioral changes.
 // Increment minor number for bug fixes and performance enhancements.
 // Increment patch number for critical fixes to existing releases.
-const DeploymentManagerVersion = "0.0.1"
+var DeploymentManagerVersion = "0.0.1"
