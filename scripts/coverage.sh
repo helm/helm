5c402f6d341835de3efb184a734a17407b872d85
Adam Reese <adam@reese.io>
2016-03-11 14:54:58 -0800

    feat(testcov): add a test coverage helper script

---
+++ scripts/coverage.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Copyright 2016 The Kubernetes Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+
+[[ "$TRACE" ]] && set -x
+
+COVERMODE=${COVERMODE:-atomic}
+PACKAGES=($(go list $(glide novendor)))
+
+echo "mode: ${COVERMODE}" > coverage.out
+
+for d in "${PACKAGES[@]}"; do
+  go test -coverprofile=profile.out -covermode="$COVERMODE" "$d"
+  if [ -f profile.out ]; then
+    sed "/mode: $COVERMODE/d" profile.out >> coverage.out
+    rm profile.out
+  fi
+done
+
+go tool cover -html=coverage.out
