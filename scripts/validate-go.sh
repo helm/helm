946fa6375cff9de8dcbab6cb963f117f432b119d
Adam Reese <adam@reese.io>
2016-03-15 10:59:16 -0600

    fix(test-style): set git repo path

--- scripts/validate-go.sh
+++ scripts/validate-go.sh
@@ -22,10 +22,12 @@ readonly    red=$(tput bold; tput setaf 1)
 readonly  green=$(tput bold; tput setaf 2)
 readonly yellow=$(tput bold; tput setaf 3)
 
+readonly REPO=github.com/kubernetes/deployment-manager
+
 exit_code=0
 
 find_go_files() {
-  git ls-files '*.go'
+  git -C "${GOPATH}/src/${REPO}" ls-files '*.go'
 }
 
 echo "==> Running golint..."
