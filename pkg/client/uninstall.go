914a341e3a62b64f85d58e7df64a99e4600f0fc7
runseb <runseb@gmail.com>
2016-03-31 08:56:36 +0200

    Use helm namespace in uninstall method

--- pkg/client/uninstall.go
+++ pkg/client/uninstall.go
@@ -25,6 +25,6 @@ import (
 // Returns the string output received from the operation, and an error if the
 // command failed.
 func Uninstall(runner kubectl.Runner) (string, error) {
-	o, err := runner.Delete("dm", "Namespace")
+	o, err := runner.Delete("helm", "Namespace")
 	return string(o), err
 }
