11e5d3e697b58d47536f5f71980f875fb66f039e
Adam Reese <adam@reese.io>
2016-04-01 08:59:24 -0700

    feat(e2e): add options as flags

--- test/e2e/helm_test.go
+++ test/e2e/helm_test.go
@@ -3,6 +3,7 @@
 package e2e
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"strings"
@@ -14,7 +15,11 @@ func init() {
 	rand.Seed(time.Now().Unix())
 }
 
-var chart = "gs://areese-charts/replicatedservice-3.tgz"
+var (
+	repoURL  = flag.String("repo-url", "gs://areese-charts", "Repository URL")
+	repoName = flag.String("repo-name", "areese-charts", "Repository name")
+	chart    = flag.String("chart", "gs://areese-charts/replicatedservice-3.tgz", "Chart to deploy")
+)
 
 func TestHelm(t *testing.T) {
 	kube := NewKubeContext()
@@ -38,10 +43,13 @@ func TestHelm(t *testing.T) {
 	t.Log("Executing deployment list")
 	helm.Run("deployment", "list")
 
+	t.Logf("Adding repo %s %s", *repoName, *repoURL)
+	helm.Run("repo", "add", *repoName, *repoURL)
+
 	deploymentName := genName()
 
 	t.Log("Executing deploy")
-	helm.Run("deploy", "--name", deploymentName, chart)
+	helm.Run("deploy", "--name", deploymentName, *chart)
 
 	t.Log("Executing deployment list")
 	helm.Run("deployment", "list")
