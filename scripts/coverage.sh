376894ff6642da3337c7c1a72d2ca054be6d2a63
Adam Reese <adam@reese.io>
2016-04-06 11:45:22 -0700

    ref(scripts): cleanup coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -17,17 +17,22 @@ set -eo pipefail
 
 [[ "$TRACE" ]] && set -x
 
+COVERDIR=${COVERDIR:-.coverage}
 COVERMODE=${COVERMODE:-atomic}
 PACKAGES=($(go list $(glide novendor)))
 
-echo "mode: ${COVERMODE}" > coverage.out
+if [[ ! -d "$COVERDIR" ]]; then
+  mkdir -p "$COVERDIR"
+fi
+
+echo "mode: ${COVERMODE}" > "${COVERDIR}/coverage.out"
 
 for d in "${PACKAGES[@]}"; do
   go test -coverprofile=profile.out -covermode="$COVERMODE" "$d"
   if [ -f profile.out ]; then
-    sed "/mode: $COVERMODE/d" profile.out >> coverage.out
+    sed "/mode: $COVERMODE/d" profile.out >> "${COVERDIR}/coverage.out"
     rm profile.out
   fi
 done
 
-go tool cover -html=coverage.out
+go tool cover -html "${COVERDIR}/coverage.out" -o "${COVERDIR}/coverage.html"
