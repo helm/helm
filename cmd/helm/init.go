9281012d3cf9e8ea1b92815738f1f0a03d152e87
Michelle Noorali <michellemolu@gmail.com>
2016-04-12 13:42:27 -0700

    feat(helm): add init cmd and test

--- cmd/helm/init.go
+++ cmd/helm/init.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -14,9 +15,11 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize Helm on both client and server.",
 	Long:  `Add long help here`,
-	Run:   runInit,
+	RunE:  RunInit,
 }
 
-func runInit(cmd *cobra.Command, args []string) {
+// RunInit initializes local config and installs tiller to Kubernetes Cluster
+func RunInit(cmd *cobra.Command, args []string) error {
 	fmt.Fprintln(stdout, "Init was called.")
+	return errors.New("NotImplemented")
 }
