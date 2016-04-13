b647e9a94d3a2d36aa22506e0f9b8208e097ff08
Matt Butcher <mbutcher@engineyard.com>
2016-04-13 11:58:24 -0600

    fix(tiller): add server test

---
+++ cmd/tiller/server_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/deis/tiller/cmd/tiller/environment"
+	"github.com/deis/tiller/pkg/engine"
+)
+
+// These are canary tests to make sure that the default server actually
+// fulfills its requirements.
+var _ environment.Engine = &engine.Engine{}
+
+func TestNewServer(t *testing.T) {
+	defer func() {
+		if recover() != nil {
+			t.Fatalf("Panic trapped. Check EngineYard.Default()")
+		}
+	}()
+	s := newServer()
+
+	// This will panic if it is not correct.
+	s.Environment.EngineYard.Default()
+
+	e, ok := s.Environment.EngineYard.Get(environment.GoTplEngine)
+	if !ok {
+		t.Fatalf("Could not find GoTplEngine")
+	}
+	if e == nil {
+		t.Fatalf("Template engine GoTplEngine returned nil.")
+	}
+}
