8afc295608b7a0d0b5170eaa4a992cc40669d87f
Michelle Noorali <michellemolu@gmail.com>
2016-04-14 17:25:00 -0600

    feat(helm): add global usage description to helm

--- cmd/helm/helm.go
+++ cmd/helm/helm.go
@@ -8,10 +8,31 @@ import (
 
 var stdout = os.Stdout
 
+var globalUsage = `The Kubernetes package manager
+
+To begin working with Helm, run the 'helm init' command:
+
+$ helm init
+
+This will install Tiller to your running Kubernetes cluster.
+It will also set up any necessary local configuration.
+
+Commond actions from this point on include:
+
+- helm search: search for charts
+- helm fetch: download a chart to your local directory to view
+- helm install: upload the chart to Kubernetes
+- helm list: list releases of charts
+
+ENVIRONMENT:
+$HELM_HOME: Set an alternative location for Helm files.
+            By default, these are stored in ~/.helm
+`
+
 var RootCommand = &cobra.Command{
 	Use:   "helm",
 	Short: "The Helm package manager for Kubernetes.",
-	Long:  `Do long help here.`,
+	Long:  globalUsage,
 }
 
 func main() {
