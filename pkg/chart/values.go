619e1e2a0a03d619bceb6323f8bacf2bf4f0349a
Matt Butcher <mbutcher@engineyard.com>
2016-04-15 16:18:46 -0600

    feat(chart): add values parser
    This adds code to parse TOML files into Values maps. These can then
    easily be passed into the template engine.
    
    Included in this is support for TOML "tables", subsections of TOML
    files. We will be using those to pass config data to dependent charts.

---
+++ pkg/chart/values.go
@@ -0,0 +1,67 @@
+package chart
+
+import (
+	"errors"
+	"io/ioutil"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+)
+
+var ErrNoTable = errors.New("no table")
+
+type Values map[string]interface{}
+
+// Table gets a table (TOML subsection) from a Values object.
+//
+// The table is returned as a Values.
+//
+// Compound table names may be specified with dots:
+//
+//	foo.bar
+//
+// The above will be evaluated as "The table bar inside the table
+// foo".
+//
+// An ErrNoTable is returned if the table does not exist.
+func (v Values) Table(name string) (Values, error) {
+	names := strings.Split(name, ".")
+	table := v
+	var err error
+
+	for _, n := range names {
+		table, err = tableLookup(table, n)
+		if err != nil {
+			return table, err
+		}
+	}
+	return table, err
+}
+
+func tableLookup(v Values, simple string) (Values, error) {
+	v2, ok := v[simple]
+	if !ok {
+		return v, ErrNoTable
+	}
+	vv, ok := v2.(map[string]interface{})
+	if !ok {
+		return vv, ErrNoTable
+	}
+	return vv, nil
+}
+
+// ReadValues will parse TOML byte data into a Values.
+func ReadValues(data []byte) (Values, error) {
+	out := map[string]interface{}{}
+	err := toml.Unmarshal(data, out)
+	return out, err
+}
+
+// ReadValuesFile will parse a TOML file into a Values.
+func ReadValuesFile(filename string) (Values, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return map[string]interface{}{}, err
+	}
+	return ReadValues(data)
+}
