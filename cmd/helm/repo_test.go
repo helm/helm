e250328e224f0ea2afbc92c6ce5c4961614bbecd
Michelle Noorali <michellemolu@gmail.com>
2016-04-25 13:10:33 -0600

    feat(helm): add helm repo add command

---
+++ cmd/helm/repo_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/deis/tiller/pkg/repo"
+)
+
+func TestRepoAdd(t *testing.T) {
+	home := createTmpHome()
+	helmHome = home
+	if err := ensureHome(); err != nil {
+		t.Errorf("%s", err)
+	}
+
+	testName := "test-name"
+	testURL := "test-url"
+	if err := insertRepoLine(testName, testURL); err != nil {
+		t.Errorf("%s", err)
+	}
+
+	f, err := repo.LoadRepositoriesFile(repositoriesFile())
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	_, ok := f.Repositories[testName]
+	if !ok {
+		t.Errorf("%s was not successfully inserted into %s", testName, repositoriesFile())
+	}
+
+	if err := insertRepoLine(testName, testURL); err == nil {
+		t.Errorf("Duplicate repository name was added")
+	}
+
+}
