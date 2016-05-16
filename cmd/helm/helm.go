228060a1bffc0a956edb2b14a0c571f5606ae3a9
Matt Butcher <mbutcher@engineyard.com>
2016-05-16 12:57:24 -0600

    fix(helm): update main help text

--- cmd/helm/helm.go
+++ cmd/helm/helm.go
@@ -19,21 +19,20 @@ var globalUsage = `The Kubernetes package manager
 
 To begin working with Helm, run the 'helm init' command:
 
-$ helm init
+	$ helm init
 
 This will install Tiller to your running Kubernetes cluster.
 It will also set up any necessary local configuration.
 
-Commond actions from this point on include:
+Common actions from this point include:
 
 - helm search: search for charts
 - helm fetch: download a chart to your local directory to view
 - helm install: upload the chart to Kubernetes
 - helm list: list releases of charts
 
-ENVIRONMENT:
-$HELM_HOME: Set an alternative location for Helm files.
-            By default, these are stored in ~/.helm
+Environment:
+  $HELM_HOME    Set an alternative location for Helm files. By default, these are stored in ~/.helm
 `
 
 // RootCommand is the top-level command for Helm.
