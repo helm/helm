792aa1a1aae1c9758ffca0985e74a38147a010f7
Adam Reese <adam@reese.io>
2016-05-24 09:55:38 -0700

    feat(kube): create tunnel for client

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -11,13 +11,13 @@ import (
 
 // Client represents a client capable of communicating with the Kubernetes API.
 type Client struct {
-	config clientcmd.ClientConfig
+	*cmdutil.Factory
 }
 
 // New create a new Client
 func New(config clientcmd.ClientConfig) *Client {
 	return &Client{
-		config: config,
+		Factory: cmdutil.NewFactory(config),
 	}
 }
 
@@ -28,22 +28,20 @@ type ResourceActorFunc func(*resource.Info) error
 //
 // Namespace will set the namespace
 func (c *Client) Create(namespace string, reader io.Reader) error {
-	f := cmdutil.NewFactory(c.config)
-	return perform(f, namespace, reader, createResource)
+	return perform(c, namespace, reader, createResource)
 }
 
 // Delete deletes kubernetes resources from an io.reader
 //
 // Namespace will set the namespace
 func (c *Client) Delete(namespace string, reader io.Reader) error {
-	f := cmdutil.NewFactory(c.config)
-	return perform(f, namespace, reader, deleteResource)
+	return perform(c, namespace, reader, deleteResource)
 }
 
 const includeThirdPartyAPIs = false
 
-func perform(f *cmdutil.Factory, namespace string, reader io.Reader, fn ResourceActorFunc) error {
-	r := f.NewBuilder(includeThirdPartyAPIs).
+func perform(c *Client, namespace string, reader io.Reader, fn ResourceActorFunc) error {
+	r := c.NewBuilder(includeThirdPartyAPIs).
 		ContinueOnError().
 		NamespaceParam(namespace).
 		RequireNamespace().
