5670d2db53723941feed3ad86f3c4624ee2043cd
Adam Reese <adam@reese.io>
2016-05-25 12:30:25 -0700

    feat(version): statically link version at build time

---
+++ pkg/version/version.go
@@ -0,0 +1,10 @@
+// Package version represents the current version of the project.
+package version
+
+// Version is the current version of the Helm.
+// Update this whenever making a new release.
+// The version is of the format Major.Minor.Patch
+// Increment major number for new feature additions and behavioral changes.
+// Increment minor number for bug fixes and performance enhancements.
+// Increment patch number for critical fixes to existing releases.
+var Version = "v2.0.0-alpha.1"
