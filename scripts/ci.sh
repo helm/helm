f89b6ce7619c876181deba1450577b0a5e796661
Adam Reese <adam@reese.io>
2016-07-05 10:55:17 -0700

    feat(ci): setup parallel testing

---
+++ scripts/ci.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Copyright 2016 The Kubernetes Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Bash 'Strict Mode'
+# http://redsymbol.net/articles/unofficial-bash-strict-mode
+set -euo pipefail
+IFS=$'\n\t'
+
+HELM_ROOT="${BASH_SOURCE[0]%/*}/.."
+cd "$HELM_ROOT"
+
+case "${CIRCLE_NODE_INDEX-0}" in
+  0)
+    echo "Running 'make test-unit'"
+    make test-unit
+    ;;
+  1)
+    echo "Running 'make test-style'"
+    make test-style
+    ;;
+  2)
+    echo "Running 'make docker-build'"
+    make test-style
+    ;;
+esac
