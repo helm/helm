aee5b4c347643757f676b236d088f93fe7a8db2d
Ville Aikas <vaikas@google.com>
2016-09-23 10:00:27 -0700

    playing with the path

--- cmd/tiller/release_server_test.go
+++ cmd/tiller/release_server_test.go
@@ -1367,7 +1367,7 @@ func mockEnvironment() *environment.Environment {
 	e := environment.New()
 	e.Releases = storage.Init(driver.NewMemory())
 	e.KubeClient = &environment.PrintingKubeClient{Out: os.Stdout}
-	e.SchemaDir = "./testdata"
+	e.SchemaDir = "cmd/tiller/testdata"
 	return e
 }
 
