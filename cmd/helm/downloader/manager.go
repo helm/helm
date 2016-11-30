a59604f40a87887942d90d754d1ebc7b6ed77b44
Matt Butcher <technosophos@gmail.com>
2016-11-30 10:26:33 -0700

    fix(helm): improve URL comparison logic
    Normalize URLs before comparing them. This deviates slightly from the
    URL spec, but in order to accomodate the predominant use pattern for
    Helm. Specifically, './', '../', and '/' are all "interpreted" to be
    filepath-like.
    
    Closes #1588

--- cmd/helm/downloader/manager.go
+++ cmd/helm/downloader/manager.go
@@ -313,12 +313,21 @@ func (m *Manager) parallelRepoUpdate(repos []*repo.Entry) {
 func urlsAreEqual(a, b string) bool {
 	au, err := url.Parse(a)
 	if err != nil {
+		a = filepath.Clean(a)
+		b = filepath.Clean(b)
 		// If urls are paths, return true only if they are an exact match
 		return a == b
 	}
 	bu, err := url.Parse(b)
 	if err != nil {
 		return false
+	}
+
+	for _, u := range []*url.URL{au, bu} {
+		if u.Path == "" {
+			u.Path = "/"
+		}
+		u.Path = filepath.Clean(u.Path)
 	}
 	return au.String() == bu.String()
 }
