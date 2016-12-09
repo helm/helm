0474bcbcf1e11ca58c02b472a4facf1e5b1e745f
Andrew Stuart <andrew.stuart2@gmail.com>
2016-12-09 13:21:48 -0700

    Document ToYaml function

--- pkg/chartutil/files.go
+++ pkg/chartutil/files.go
@@ -146,6 +146,10 @@ func (f Files) AsSecrets() string {
 	return ToYaml(m)
 }
 
+// ToYaml takes an interface, marshals it to yaml, and returns a string. It will
+// always return a string, even on marshal error (empty string).
+//
+// This is designed to be called from a template.
 func ToYaml(v interface{}) string {
 	data, err := yaml.Marshal(v)
 	if err != nil {
