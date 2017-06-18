76e6e15a0c89c03fe6cfa0ac48b7f3cc1f2e5cb5
Reinhard Nägele <unguiculus@gmail.com>
2017-06-18 14:42:36 +0200

    Add comment on resources

--- pkg/chartutil/create.go
+++ pkg/chartutil/create.go
@@ -75,13 +75,16 @@ ingress:
     #   hosts:
     #     - chart-example.local
 resources: {}
+  # We usually recommend not to specify default resources and to leave this as a conscious 
+  # choice for the user. This also increases chances charts run on environments with little 
+  # resources, such as Minikube. If you do want to specify resources, uncomment the following 
+  # lines, adjust them as necessary, and remove the curly braces after 'resources:'.
   # limits:
   #  cpu: 100m
   #  memory: 128Mi
   #requests:
   #  cpu: 100m
   #  memory: 128Mi
-
 `
 
 const defaultIgnore = `# Patterns to ignore when building packages.
