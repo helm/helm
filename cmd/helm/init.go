35c7742330a06e5b56a368b91ff4bb7fa77093ab
Ryan Payton <ryan.payton@vungle.com>
2017-09-07 13:33:37 -0700

    reverted initLocalRepo call to use CacheIndex since RelativeIndex was causing 'make test' to create a symlink in testing directory

--- cmd/helm/init.go
+++ cmd/helm/init.go
@@ -344,7 +344,7 @@ func ensureDefaultRepos(home helmpath.Home, out io.Writer, skipRefresh bool) err
 		if err != nil {
 			return err
 		}
-		lr, err := initLocalRepo(home.LocalRepository(localRepositoryIndexFile), home.RelativeIndex("local"), out)
+		lr, err := initLocalRepo(home.LocalRepository(localRepositoryIndexFile), home.CacheIndex("local"), out)
 		if err != nil {
 			return err
 		}
