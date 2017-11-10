c864ba179ee97ac111529fa8e6285e0a3e40760c
Maxime Guyot <Miouge1@users.noreply.github.com>
2017-11-10 10:52:56 +0100

    Add fullnameOverride

--- pkg/chartutil/create.go
+++ pkg/chartutil/create.go
@@ -240,7 +240,8 @@ We truncate at 63 chars because some Kubernetes name fields are limited to this 
 */}}
 {{- define "<CHARTNAME>.fullname" -}}
 {{- $name := default .Chart.Name .Values.nameOverride -}}
-{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- $default := printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- default $default .Values.fullnameOverride -}}
 {{- end -}}
 `
 
