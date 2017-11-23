f432e07126524db74fe5b9c958d07c74cbb7796e
Alexandr Burdiyan <burdiyan@gmail.com>
2017-11-23 11:19:48 +0100

    ref(helm): improve fullname template in starter chart
    Closes #1186

--- pkg/chartutil/create.go
+++ pkg/chartutil/create.go
@@ -237,11 +237,15 @@ Expand the name of the chart.
 {{/*
 Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
+If release name contains chart name it will be used as a full name.
 */}}
 {{- define "<CHARTNAME>.fullname" -}}
 {{- $name := default .Chart.Name .Values.nameOverride -}}
-{{- $default := printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
-{{- default $default .Values.fullnameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
 {{- end -}}
 `
 
