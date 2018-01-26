1e0906ed67c08f754cc28c7047eea228cb76e373
Bin Liu <liubin@users.noreply.github.com>
2018-01-26 18:41:23 +0800

    Fix type

--- pkg/helm/fake.go
+++ pkg/helm/fake.go
@@ -184,7 +184,7 @@ func (c *FakeClient) RunReleaseTest(rlsName string, opts ...ReleaseTestOption) (
 	return results, errc
 }
 
-// PingTiller pings the Tiller pod and ensure's that it is up and runnning
+// PingTiller pings the Tiller pod and ensure's that it is up and running
 func (c *FakeClient) PingTiller() error {
 	return nil
 }
