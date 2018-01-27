7086a16e29cfb106c272437f756b6cef628e7582
Reinhard Nägele <unguiculus@gmail.com>
2018-01-27 11:18:41 +0100

    Fix 'getSelectorFromObject'
    (cherry picked from commit ea520afd3e4d0eb3637a10b7443ca83bcaf340e8)

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -27,10 +27,12 @@ import (
 	"time"
 
 	jsonpatch "github.com/evanphx/json-patch"
-	apps "k8s.io/api/apps/v1beta2"
+	appsv1 "k8s.io/api/apps/v1"
+	appsv1beta1 "k8s.io/api/apps/v1beta1"
+	appsv1beta2 "k8s.io/api/apps/v1beta2"
 	batch "k8s.io/api/batch/v1"
 	"k8s.io/api/core/v1"
-	"k8s.io/api/extensions/v1beta1"
+	extv1beta1 "k8s.io/api/extensions/v1beta1"
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -507,18 +509,41 @@ func updateResource(c *Client, target *resource.Info, currentObj runtime.Object,
 
 func getSelectorFromObject(obj runtime.Object) (map[string]string, error) {
 	switch typed := obj.(type) {
+
 	case *v1.ReplicationController:
 		return typed.Spec.Selector, nil
-	case *v1beta1.ReplicaSet:
+
+	case *extv1beta1.ReplicaSet:
 		return typed.Spec.Selector.MatchLabels, nil
-	case *v1beta1.Deployment:
+	case *appsv1.ReplicaSet:
 		return typed.Spec.Selector.MatchLabels, nil
-	case *v1beta1.DaemonSet:
+
+	case *extv1beta1.Deployment:
 		return typed.Spec.Selector.MatchLabels, nil
+	case *appsv1beta1.Deployment:
+		return typed.Spec.Selector.MatchLabels, nil
+	case *appsv1beta2.Deployment:
+		return typed.Spec.Selector.MatchLabels, nil
+	case *appsv1.Deployment:
+		return typed.Spec.Selector.MatchLabels, nil
+
+	case *extv1beta1.DaemonSet:
+		return typed.Spec.Selector.MatchLabels, nil
+	case *appsv1beta2.DaemonSet:
+		return typed.Spec.Selector.MatchLabels, nil
+	case *appsv1.DaemonSet:
+		return typed.Spec.Selector.MatchLabels, nil
+
 	case *batch.Job:
 		return typed.Spec.Selector.MatchLabels, nil
-	case *apps.StatefulSet:
+
+	case *appsv1beta1.StatefulSet:
 		return typed.Spec.Selector.MatchLabels, nil
+	case *appsv1beta2.StatefulSet:
+		return typed.Spec.Selector.MatchLabels, nil
+	case *appsv1.StatefulSet:
+		return typed.Spec.Selector.MatchLabels, nil
+
 	default:
 		return nil, fmt.Errorf("Unsupported kind when getting selector: %v", obj)
 	}
