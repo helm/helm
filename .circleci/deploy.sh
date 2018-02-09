f7e078ca29d4b063f49e9dffda8412a2a3de1085
Matthew Fisher <matt.fisher@microsoft.com>
2018-02-09 13:53:38 -0800

    bump docker to 17.11.0-ce

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -34,7 +34,7 @@ else
 fi
 
 echo "Install docker client"
-VER="17.03.0-ce"
+VER="17.11.0-ce"
 curl -L -o /tmp/docker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz
 tar -xz -C /tmp -f /tmp/docker-$VER.tgz
 mv /tmp/docker/* /usr/bin
