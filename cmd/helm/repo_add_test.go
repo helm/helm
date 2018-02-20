4198c7ce87e48c1e0d4911b0a24ccf38a19b2594
Arash Deshmeh <adeshmeh@ca.ibm.com>
2018-02-20 11:45:46 -0500

    fix(helm): fix the bug in tests under cmd/helm that leaves behind temporary directories named "repo-test-*" during build.
    Signed-off-by: Arash Deshmeh <adeshmeh@ca.ibm.com>

--- cmd/helm/repo_add_test.go
+++ cmd/helm/repo_add_test.go
@@ -36,7 +36,7 @@ func TestRepoAddCmd(t *testing.T) {
 	cleanup := resetEnv()
 	defer func() {
 		srv.Stop()
-		os.Remove(thome.String())
+		os.RemoveAll(thome.String())
 		cleanup()
 	}()
 	if err := ensureTestHome(thome, t); err != nil {
@@ -72,7 +72,7 @@ func TestRepoAdd(t *testing.T) {
 	hh := thome
 	defer func() {
 		ts.Stop()
-		os.Remove(thome.String())
+		os.RemoveAll(thome.String())
 		cleanup()
 	}()
 	if err := ensureTestHome(hh, t); err != nil {
