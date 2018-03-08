92da302be5ae85488fcd2bd2c236c206c9bc0ce4
Adam Reese <adam@reese.io>
2018-03-08 11:43:59 -0800

    install docker client during bootstrap

--- .circleci/bootstrap.sh
+++ .circleci/bootstrap.sh
@@ -16,4 +16,11 @@
 set -euo pipefail
 
 apt-get update -y && apt-get install -yq zip socat
+
+echo "Install docker client"
+VER="17.09.0-ce"
+curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/x86_64/docker-$VER.tgz
+tar -xz -C /tmp -f /tmp/docker-$VER.tgz
+mv /tmp/docker/* /usr/bin
+
 make bootstrap
