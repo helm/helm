fb6645f67ee50aad32c64a32701fecedd2660412
Arturo Contreras <arturo.contreras@returnpath.com>
2018-03-14 16:33:02 -0600

    Adding --set-string flag to force string values.

--- pkg/strvals/parser_test.go
+++ pkg/strvals/parser_test.go
@@ -65,6 +65,17 @@ func TestSetIndex(t *testing.T) {
 }
 
 func TestParseSet(t *testing.T) {
+	testsString := []struct {
+		str    string
+		expect map[string]interface{}
+		err    bool
+	}{
+		{
+			str:    "long_int_string=1234567890",
+			expect: map[string]interface{}{"long_int_string": "1234567890"},
+			err:    false,
+		},
+	}
 	tests := []struct {
 		str    string
 		expect map[string]interface{}
@@ -96,6 +107,10 @@ func TestParseSet(t *testing.T) {
 		{
 			str:    "leading_zeros=00009",
 			expect: map[string]interface{}{"leading_zeros": "00009"},
+		},
+		{
+			str:    "long_int=1234567890",
+			expect: map[string]interface{}{"long_int": 1234567890},
 		},
 		{
 			str: "name1,name2=",
@@ -259,6 +274,31 @@ func TestParseSet(t *testing.T) {
 
 	for _, tt := range tests {
 		got, err := Parse(tt.str)
+		if err != nil {
+			if tt.err {
+				continue
+			}
+			t.Fatalf("%s: %s", tt.str, err)
+		}
+		if tt.err {
+			t.Errorf("%s: Expected error. Got nil", tt.str)
+		}
+
+		y1, err := yaml.Marshal(tt.expect)
+		if err != nil {
+			t.Fatal(err)
+		}
+		y2, err := yaml.Marshal(got)
+		if err != nil {
+			t.Fatalf("Error serializing parsed value: %s", err)
+		}
+
+		if string(y1) != string(y2) {
+			t.Errorf("%s: Expected:\n%s\nGot:\n%s", tt.str, y1, y2)
+		}
+	}
+	for _, tt := range testsString {
+		got, err := ParseString(tt.str)
 		if err != nil {
 			if tt.err {
 				continue
