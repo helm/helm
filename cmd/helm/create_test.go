e0056a9e41d5432192ee4d103a72e36ba9aeec36
Arash Deshmeh <adeshmeh@ca.ibm.com>
2018-03-29 05:08:56 -0400

    fix(helm): fix output leak from unit tests of helm create command
    Signed-off-by: Arash Deshmeh <adeshmeh@ca.ibm.com>

--- cmd/helm/create_test.go
+++ cmd/helm/create_test.go
@@ -46,7 +46,7 @@ func TestCreateCmd(t *testing.T) {
 	defer os.Chdir(pwd)
 
 	// Run a create
-	cmd := newCreateCmd(os.Stdout)
+	cmd := newCreateCmd(ioutil.Discard)
 	if err := cmd.RunE(cmd, []string{cname}); err != nil {
 		t.Errorf("Failed to run create: %s", err)
 		return
@@ -117,7 +117,7 @@ func TestCreateStarterCmd(t *testing.T) {
 	defer os.Chdir(pwd)
 
 	// Run a create
-	cmd := newCreateCmd(os.Stdout)
+	cmd := newCreateCmd(ioutil.Discard)
 	cmd.ParseFlags([]string{"--starter", "starterchart"})
 	if err := cmd.RunE(cmd, []string{cname}); err != nil {
 		t.Errorf("Failed to run create: %s", err)
