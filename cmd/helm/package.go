7abed941fb9a14cc1b4dbd2b4bdf906c6e9ad41b
Matthew Fisher <matt.fisher@microsoft.com>
2018-04-16 16:46:47 -0700

    Revert "feat: add --set and --values options to 'helm package'"
    This reverts commit a930eb7ff4dcba13831174496dda2445e81ebeaa.

--- cmd/helm/package.go
+++ cmd/helm/package.go
@@ -53,9 +53,6 @@ type packageCmd struct {
 	save             bool
 	sign             bool
 	path             string
-	valueFiles       valueFiles
-	values           []string
-	stringValues     []string
 	key              string
 	keyring          string
 	version          string
@@ -98,9 +95,6 @@ func newPackageCmd(out io.Writer) *cobra.Command {
 	}
 
 	f := cmd.Flags()
-	f.VarP(&pkg.valueFiles, "values", "f", "specify values in a YAML file or a URL(can specify multiple)")
-	f.StringArrayVar(&pkg.values, "set", []string{}, "set values on the command line (can specify multiple or separate values with commas: key1=val1,key2=val2)")
-	f.StringArrayVar(&pkg.stringValues, "set-string", []string{}, "set STRING values on the command line (can specify multiple or separate values with commas: key1=val1,key2=val2)")
 	f.BoolVar(&pkg.save, "save", true, "save packaged chart to local chart repository")
 	f.BoolVar(&pkg.sign, "sign", false, "use a PGP private key to sign this package")
 	f.StringVar(&pkg.key, "key", "", "name of the key to use when signing. Used if --sign is true")
@@ -138,20 +132,6 @@ func (p *packageCmd) run() error {
 	if err != nil {
 		return err
 	}
-
-	overrideVals, err := vals(p.valueFiles, p.values, p.stringValues)
-	if err != nil {
-		return err
-	}
-	combinedVals, err := chartutil.CoalesceValues(ch, &chart.Config{Raw: string(overrideVals)})
-	if err != nil {
-		return err
-	}
-	newVals, err := combinedVals.YAML()
-	if err != nil {
-		return err
-	}
-	ch.Values = &chart.Config{Raw: newVals}
 
 	// If version is set, modify the version.
 	if len(p.version) != 0 {
