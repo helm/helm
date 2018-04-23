bcf5688e9a9da64fe5c8580c435bd248ec499e99
AdamDang <adam.dang@daocloud.io>
2018-04-23 13:06:24 +0800

    Update install.go

--- cmd/helm/installer/install.go
+++ cmd/helm/installer/install.go
@@ -73,7 +73,7 @@ func Upgrade(client kubernetes.Interface, opts *Options) error {
 	if _, err := client.ExtensionsV1beta1().Deployments(opts.Namespace).Update(obj); err != nil {
 		return err
 	}
-	// If the service does not exists that would mean we are upgrading from a Tiller version
+	// If the service does not exist that would mean we are upgrading from a Tiller version
 	// that didn't deploy the service, so install it.
 	_, err = client.CoreV1().Services(opts.Namespace).Get(serviceName, metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
