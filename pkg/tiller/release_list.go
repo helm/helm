8d408876a059223c9a29d99efb35b3497a53504c
Matthew Fisher <matt.fisher@microsoft.com>
2018-09-10 11:14:03 -0700

    Fix race condition in `helm list` (#4620)
    * Fix race in helm list when partitioning
    
    Problem:
    The chunks slice that is passed through the channel is reused for each
    partition. This means that encoding the release into a message is racing with
    populating the next partition, causing the results to sometimes not fit in the
    message, and the release list to be incorrect
    
    Solution:
    Allocate a new slice for each partition
    
    Issue #3322
    
    Signed-off-by: Brian Marshall <bmarshall13@users.noreply.github.com>
    (cherry picked from commit a0858e29d877b8aa19478133865e45958ff21a9e)
    
    * fix import sorting
    
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>
    
    * ref(release_server_test): use NewReleaseServer()
    
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>
    
    * add unit test for race condition in `helm list`
    
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>
    (cherry picked from commit 5b236324468ad8958c351dd3c734880c0fa5d561)

--- pkg/tiller/release_list.go
+++ pkg/tiller/release_list.go
@@ -18,11 +18,12 @@ package tiller
 
 import (
 	"fmt"
+	"regexp"
+
 	"github.com/golang/protobuf/proto"
 	"k8s.io/helm/pkg/proto/hapi/release"
 	"k8s.io/helm/pkg/proto/hapi/services"
 	relutil "k8s.io/helm/pkg/releaseutil"
-	"regexp"
 )
 
 // ListReleases lists the releases found by the server.
@@ -140,7 +141,7 @@ func (s *ReleaseServer) partition(rels []*release.Release, cap int) <-chan []*re
 				s.Log("partitioned at %d with %d releases (cap=%d)", fill, len(chunk), cap)
 				chunks <- chunk
 				// reset paritioning state
-				chunk = chunk[:0]
+				chunk = nil
 				fill = 0
 			}
 			chunk = append(chunk, rls)
