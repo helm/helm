f98366fc50fc00ad1e18dd4202d244966e07c5aa
Bartel Sielski <bartel.sielski@gmail.com>
2018-10-30 17:24:20 +0100

    Remove newline at the start of zsh completion file (#4851)
    Signed-off-by: Bartel Sielski <bartel.sielski@gmail.com>
    (cherry picked from commit 1ebbd6989645375abd9268f9b3315e296b52f828)

--- cmd/helm/completion.go
+++ cmd/helm/completion.go
@@ -81,7 +81,8 @@ func runCompletionBash(out io.Writer, cmd *cobra.Command) error {
 }
 
 func runCompletionZsh(out io.Writer, cmd *cobra.Command) error {
-	zshInitialization := `
+	zshInitialization := `#compdef helm
+
 __helm_bash_source() {
 	alias shopt=':'
 	alias _expand=_bash_expand
