55ea6987841b240ce65d6e7231ddb8046b4369c0
Brice Rising <brice.rising@slalom.com>
2018-12-17 16:10:55 -0500

    Add Apache License line to fsutil
    This is probably not the correct apporach to using bsd licensed code under an apache license.
    
    Signed-off-by: Brice Rising <brice.rising@slalom.com>

--- pkg/fsutil/rename.go
+++ pkg/fsutil/rename.go
@@ -1,6 +1,8 @@
 // Copyright 2016 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
 
 // +build !windows
 
