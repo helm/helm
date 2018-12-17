85b5e04c706562ef299ac947dd1b843d5f8fc19a
Brice Rising <brice.rising@slalom.com>
2018-12-17 16:17:05 -0500

    Copy approach done in another project for using bsd license
    I found another package that took code from a BSD licensed open source project. I copied their approach for reusing that license.
    
    Signed-off-by: Brice Rising <brice.rising@slalom.com>

--- pkg/fsutil/fs.go
+++ pkg/fsutil/fs.go
@@ -1,8 +1,22 @@
-// Copyright 2016 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license.
+/*
+Copyright (c) for portions of fs.go are held by The Go Authors, 2016 and are provided under
+the BSD license.
 
-// Licensed under the Apache License, Version 2.0 (the "License");
+https://github.com/golang/go/blob/master/LICENSE
+
+Copyright The Helm Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package fs
 
