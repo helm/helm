66cdae0bd03e36b8bd2f046d10442db80aa3950a
Geoff Baskwill <me@geoffbaskwill.ca>
2019-01-22 10:03:01 -0500

    fix: ignore pax header "file"s in chart validation
    Signed-off-by: Geoff Baskwill <me@geoffbaskwill.ca>
    
    Signed-off-by: Kevin Labesse <kevin@labesse.me>

--- pkg/chartutil/load.go
+++ pkg/chartutil/load.go
@@ -93,6 +93,12 @@ func loadArchiveFiles(in io.Reader) ([]*BufferedFile, error) {
 			continue
 		}
 
+		switch hd.Typeflag {
+		// We don't want to process these extension header files.
+		case tar.TypeXGlobalHeader, tar.TypeXHeader:
+			continue
+		}
+
 		// Archive could contain \ if generated on Windows
 		delimiter := "/"
 		if strings.ContainsRune(hd.Name, '\\') {
