35b782219041d39a56557728b5b6933cca7178d9
Matt Farina <matt@mattfarina.com>
2019-02-15 12:48:46 -0500

    Noting the version as the match element
    That the match is based on version is implicit. Making it explicit
    because I had to think when I saw it.
    
    Signed-off-by: Matt Farina <matt@mattfarina.com>

--- pkg/downloader/chart_downloader.go
+++ pkg/downloader/chart_downloader.go
@@ -213,7 +213,7 @@ func (c *ChartDownloader) ResolveChartVersion(ref, version string) (*url.URL, ge
 
 	cv, err := i.Get(chartName, version)
 	if err != nil {
-		return u, r.Client, fmt.Errorf("chart %q matching %q not found in %s index. (try 'helm repo update'). %s", chartName, version, r.Config.Name, err)
+		return u, r.Client, fmt.Errorf("chart %q matching version %q not found in %s index. (try 'helm repo update'). %s", chartName, version, r.Config.Name, err)
 	}
 
 	if len(cv.URLs) == 0 {
