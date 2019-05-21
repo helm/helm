3708fad5170670dfa650b13e5d10fc2521636661
dev-chulbuji <dev.chulbuji@gmail.com>
2019-05-22 00:54:15 +0900

    feat: change miss fixing Signed-off-by: dev-chulbuji <dev.chulbuji@gmail.com>

--- pkg/kube/client_test.go
+++ pkg/kube/client_test.go
@@ -27,7 +27,7 @@ import (
 	"time"
 
 	"k8s.io/api/core/v1"
-	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiapps/v1"
+	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
