9014bd9c502d92fb78bfee553abdba5cf35e878f
Oleg Sidorov <oleg.sidorov@booking.com>
2019-07-11 15:12:02 +0200

    chartutil.ReadValues is forced to unmarshal numbers into json.Number
    This change is an attempt to address the common problem of json number
    unmarshalling where any number is converted into a float64 and
    represented in a scientific notation on a marshall call. This behavior
    breaks things like: chart versions and image tags if not converted to
    yaml strings explicitly.
    
    An example of this behavior: k8s failure to fetch an image tagged with a
    big number like: $IMAGE:20190612073634 after a few steps of yaml
    re-rendering turns into: $IMAGE:2.0190612073634e+13.
    
    Example issue: https://github.com/helm/helm/issues/1707
    
    This commit forces yaml parser to use JSON modifiers and explicitly
    enables interface{} unmarshalling instead of float64. The change
    introduced might be breaking so should be processed with an extra care.
    
    Due to the fact helm mostly dals with human-produced data (charts), we
    have a decent level of confidence this change looses no functionality
    helm users rely upon (the scientific notation).
    
    Relevant doc: https://golang.org/pkg/encoding/json/#Decoder.UseNumber
    
    Signed-off-by: Oleg Sidorov <oleg.sidorov@booking.com>

--- pkg/chartutil/requirements_test.go
+++ pkg/chartutil/requirements_test.go
@@ -15,6 +15,7 @@ limitations under the License.
 package chartutil
 
 import (
+	"encoding/json"
 	"os"
 	"path/filepath"
 	"sort"
@@ -302,6 +303,10 @@ func verifyRequirementsImportValues(t *testing.T, c *chart.Chart, v *chart.Confi
 		}
 
 		switch pv.(type) {
+		case json.Number:
+			if s := pv.(json.Number).String(); s != vv {
+				t.Errorf("Failed to match imported number value %v with expected %v", s, vv)
+			}
 		case float64:
 			s := strconv.FormatFloat(pv.(float64), 'f', -1, 64)
 			if s != vv {
