58043d8657d7b5e1c8a23bdc16dc025e7227226f
Efrat19 <efrat890089@gmail.com>
2019-10-06 21:47:20 +0300

    helm plugin subcommands aliases
    Signed-off-by: Efrat19 <efrat890089@gmail.com>

--- cmd/helm/plugin_list.go
+++ cmd/helm/plugin_list.go
@@ -25,7 +25,8 @@ import (
 
 func newPluginListCmd(out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "list",
+		Use:   "list",		
+		Aliases: []string{"ls"},
 		Short: "list installed Helm plugins",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			debug("pluginDirs: %s", settings.PluginsDirectory)
