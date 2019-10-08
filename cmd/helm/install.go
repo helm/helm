41caa936b425517c6b799c1355f5eb340420dfe9
rimas <rmocius@gmail.com>
2019-10-08 13:18:58 +0100

    Remove Tiller reference from --install
    Signed-off-by: rimas <rmocius@gmail.com>

--- cmd/helm/install.go
+++ cmd/helm/install.go
@@ -74,8 +74,7 @@ set for a key called 'foo', the 'newbar' value would take precedence:
 
 
 To check the generated manifests of a release without installing the chart,
-the '--debug' and '--dry-run' flags can be combined. This will still require a
-round-trip to the Tiller server.
+the '--debug' and '--dry-run' flags can be combined.
 
 If --verify is set, the chart MUST have a provenance file, and the provenance
 file MUST pass all verification steps.
