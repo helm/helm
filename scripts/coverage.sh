b048f8914b08c42f77bbf40158cb00435aa8f7dd
Matthew Fisher <matt.fisher@microsoft.com>
2019-10-22 13:52:30 -0700

    fix(coverage): cd to root dir to avoid adding godir to go modules
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -20,8 +20,10 @@ covermode=${COVERMODE:-atomic}
 coverdir=$(mktemp -d /tmp/coverage.XXXXXXXXXX)
 profile="${coverdir}/cover.out"
 
+pushd /
 hash goveralls 2>/dev/null || go get github.com/mattn/goveralls
 hash godir 2>/dev/null || go get github.com/Masterminds/godir
+popd
 
 generate_cover_data() {
   for d in $(godir) ; do
