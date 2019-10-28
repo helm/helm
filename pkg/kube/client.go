23c82fc3ebd954b56103f28edade68779e58feec
Adam Reese <adam@reese.io>
2019-10-28 14:51:14 -0700

    ref(glide.yaml): kubernetes 1.16.2
    Signed-off-by: Adam Reese <adam@reese.io>

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -55,11 +55,11 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	cachetools "k8s.io/client-go/tools/cache"
 	watchtools "k8s.io/client-go/tools/watch"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"k8s.io/kubectl/pkg/validation"
 	"k8s.io/kubernetes/pkg/api/legacyscheme"
 	"k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/kubernetes/pkg/kubectl/cmd/get"
-	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
-	"k8s.io/kubernetes/pkg/kubectl/validation"
 )
 
 // MissingGetHeader is added to Get's output when a resource is not found.
