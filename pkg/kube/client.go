421de8dcf0db63e09fe9edede1ec69eb438ff535
kubanczyk <kubanczyk@noreply.users.github.com>
2019-11-14 22:02:19 +0100

    fix(pkg) move AddToScheme() into init()
    Closes #6566
    Signed-off-by: Jakub Bielecki <jakub.bielecki@codilime.com>

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -54,15 +54,18 @@ type Client struct {
 	Log     func(string, ...interface{})
 }
 
+// At every startup add CRDs to the scheme. They are missing by default.
+func init() {
+	if err := apiextv1beta1.AddToScheme(scheme.Scheme); err != nil {
+		// This should never happen.
+		panic(err)
+	}
+}
+
 // New creates a new Client.
 func New(getter genericclioptions.RESTClientGetter) *Client {
 	if getter == nil {
 		getter = genericclioptions.NewConfigFlags(true)
-	}
-	// Add CRDs to the scheme. They are missing by default.
-	if err := apiextv1beta1.AddToScheme(scheme.Scheme); err != nil {
-		// This should never happen.
-		panic(err)
 	}
 	return &Client{
 		Factory: cmdutil.NewFactory(getter),
