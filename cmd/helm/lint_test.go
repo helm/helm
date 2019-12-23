bc45d6531207f0a2e223cebb4500cacb936452f7
Nick Lee <nmkyu.lee@gmail.com>
2019-12-22 20:04:15 -0500

    test(cmd/lint): added test for --with-subcharts flag
    Signed-off-by: Nick Lee <nmkyu.lee@gmail.com>

---
+++ cmd/helm/lint_test.go
@@ -0,0 +1,38 @@
+/*
+Copyright The Helm Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLintCmdWithSubchartsFlag(t *testing.T) {
+	testChart := "testdata/testcharts/chart-with-bad-subcharts"
+	tests := []cmdTestCase{{
+		name:      "lint good chart with bad subcharts",
+		cmd:       fmt.Sprintf("lint %s", testChart),
+		golden:    "output/lint-chart-with-bad-subcharts.txt",
+		wantError: false,
+	}, {
+		name:      "lint good chart with bad subcharts using --with-subcharts flag",
+		cmd:       fmt.Sprintf("lint --with-subcharts %s", testChart),
+		golden:    "output/lint-chart-with-bad-subcharts-with-subcharts.txt",
+		wantError: true,
+	}}
+	runTestCmd(t, tests)
+}
