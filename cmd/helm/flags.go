a58430a944454ff8e5a560a9d3d70f5e05385310
Anton Kvashenkin <anton.jugatsu@gmail.com>
2020-01-02 19:32:50 +0400

    Fix typo in --values cmd flag
    Fix a small typo in `--values` flag in `helm install/upgrade --help` output.
    
    Signed-off-by: Anton Kvashenkin <anton.jugatsu@gmail.com>

--- cmd/helm/flags.go
+++ cmd/helm/flags.go
@@ -31,7 +31,7 @@ import (
 const outputFlag = "output"
 
 func addValueOptionsFlags(f *pflag.FlagSet, v *values.Options) {
-	f.StringSliceVarP(&v.ValueFiles, "values", "f", []string{}, "specify values in a YAML file or a URL(can specify multiple)")
+	f.StringSliceVarP(&v.ValueFiles, "values", "f", []string{}, "specify values in a YAML file or a URL (can specify multiple)")
 	f.StringArrayVar(&v.Values, "set", []string{}, "set values on the command line (can specify multiple or separate values with commas: key1=val1,key2=val2)")
 	f.StringArrayVar(&v.StringValues, "set-string", []string{}, "set STRING values on the command line (can specify multiple or separate values with commas: key1=val1,key2=val2)")
 	f.StringArrayVar(&v.FileValues, "set-file", []string{}, "set values from respective files specified via the command line (can specify multiple or separate values with commas: key1=path1,key2=path2)")
