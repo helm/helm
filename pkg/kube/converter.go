a3fe18ac5e463567db84254e0572cbe47ac78daa
Hidde Beydals <hello@hidde.co>
2020-02-11 23:52:00 +0100

    fix(kube): generate k8s native scheme only once
    Signed-off-by: Hidde Beydals <hello@hidde.co>
    Signed-off-by: Matheus Hunsche <matheus.hunsche@ifood.com.br>

--- pkg/kube/converter.go
+++ pkg/kube/converter.go
@@ -17,15 +17,19 @@ limitations under the License.
 package kube // import "helm.sh/helm/v3/pkg/kube"
 
 import (
+	"sync"
+
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/cli-runtime/pkg/resource"
 	"k8s.io/client-go/kubernetes/scheme"
 )
+
+var k8sNativeScheme *runtime.Scheme
+var k8sNativeSchemeOnce sync.Once
 
 // AsVersioned converts the given info into a runtime.Object with the correct
 // group and version set
@@ -53,11 +57,13 @@ func convertWithMapper(obj runtime.Object, mapping *meta.RESTMapping) runtime.Ob
 // combination with e.g. a versioned kube client. If we would not do this, the client
 // may attempt to perform e.g. a 3-way-merge strategy patch for custom resources.
 func kubernetesNativeScheme() *runtime.Scheme {
-	s := runtime.NewScheme()
-	utilruntime.Must(scheme.AddToScheme(s))
-	// API extensions are not in the above scheme set,
-	// and must thus be added separately.
-	utilruntime.Must(apiextensionsv1beta1.AddToScheme(s))
-	utilruntime.Must(apiextensionsv1.AddToScheme(s))
-	return s
+	k8sNativeSchemeOnce.Do(func() {
+		k8sNativeScheme = runtime.NewScheme()
+		scheme.AddToScheme(k8sNativeScheme)
+		// API extensions are not in the above scheme set,
+		// and must thus be added separately.
+		apiextensionsv1beta1.AddToScheme(k8sNativeScheme)
+		apiextensionsv1.AddToScheme(k8sNativeScheme)
+	})
+	return k8sNativeScheme
 }
