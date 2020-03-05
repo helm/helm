430474db74d7991629dba99d4b21a9c90155195a
Dong Gang <dong.gang@daocloud.io>
2020-03-05 09:51:31 +0800

    fix(helm): report User-Agent to kubernetes calls
    Close #7719
    
    Signed-off-by: Dong Gang <dong.gang@daocloud.io>

--- pkg/kube/config.go
+++ pkg/kube/config.go
@@ -19,8 +19,8 @@ package kube // import "helm.sh/helm/v3/pkg/kube"
 import "k8s.io/cli-runtime/pkg/genericclioptions"
 
 // GetConfig returns a Kubernetes client config.
-func GetConfig(kubeconfig, context, namespace string) *genericclioptions.ConfigFlags {
-	cf := genericclioptions.NewConfigFlags(true)
+func GetConfig(kubeconfig, context, namespace string) *ConfigFlags {
+	cf := &ConfigFlags{*genericclioptions.NewConfigFlags(true)}
 	cf.Namespace = &namespace
 	cf.Context = &context
 	cf.KubeConfig = &kubeconfig
