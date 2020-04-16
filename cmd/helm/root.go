21d2aa7f2b28e0ecec0cd17825a422a692c71752
Elliot Maincourt <e.maincourt@gmail.com>
2020-04-16 22:53:40 +0200

    Migrate SQL storage driver to Helm 3 (#7635)
    * Migrate SQL storage driver to Helm 3
    
    Signed-off-by: Elliot Maincourt <e.maincourt@gmail.com>
    
    * Update pkg/storage/driver/sql.go
    
    Co-Authored-By: Sebastian Pöhn <sebastian.poehn@gmail.com>
    Signed-off-by: Elliot Maincourt <e.maincourt@gmail.com>
    
    * Add authentication to releases_v3
    
    Signed-off-by: Elliot Maincourt <e.maincourt@gmail.com>
    
    * Fix migration
    
    Signed-off-by: Elliot Maincourt <e.maincourt@gmail.com>
    
    * Template the init migration
    
    Signed-off-by: Elliot Maincourt <e.maincourt@gmail.com>
    
    * Prevent potential SQL injection
    
    Signed-off-by: Elliot Maincourt <e.maincourt@gmail.com>
    
    * Use an SQL querybuilder
    
    Signed-off-by: Elliot Maincourt <e.maincourt@gmail.com>
    
    * Remove references to HELM_DRIVER_SQL_DIALECT
    
    Signed-off-by: Elliot Maincourt <e.maincourt@gmail.com>
    
    Co-authored-by: Sebastian Pöhn <sebastian.poehn@gmail.com>
    Co-authored-by: Matt Butcher <matt.butcher@microsoft.com>

--- cmd/helm/root.go
+++ cmd/helm/root.go
@@ -43,16 +43,17 @@ Common actions for Helm:
 
 Environment variables:
 
-+------------------+-----------------------------------------------------------------------------+
-| Name             | Description                                                                 |
-+------------------+-----------------------------------------------------------------------------+
-| $XDG_CACHE_HOME  | set an alternative location for storing cached files.                       |
-| $XDG_CONFIG_HOME | set an alternative location for storing Helm configuration.                 |
-| $XDG_DATA_HOME   | set an alternative location for storing Helm data.                          |
-| $HELM_DRIVER     | set the backend storage driver. Values are: configmap, secret, memory       |
-| $HELM_NO_PLUGINS | disable plugins. Set HELM_NO_PLUGINS=1 to disable plugins.                  |
-| $KUBECONFIG      | set an alternative Kubernetes configuration file (default "~/.kube/config") |
-+------------------+-----------------------------------------------------------------------------+
++------------------+--------------------------------------------------------------------------------------------------------+
+| Name                                  | Description                                                                       |
++------------------+--------------------------------------------------------------------------------------------------------+
+| $XDG_CACHE_HOME                       | set an alternative location for storing cached files.                             |
+| $XDG_CONFIG_HOME                      | set an alternative location for storing Helm configuration.                       |
+| $XDG_DATA_HOME                        | set an alternative location for storing Helm data.                                |
+| $HELM_DRIVER                          | set the backend storage driver. Values are: configmap, secret, memory, postgres   |
+| $HELM_DRIVER_SQL_CONNECTION_STRING    | set the connection string the SQL storage driver should use.                      |
+| $HELM_NO_PLUGINS                      | disable plugins. Set HELM_NO_PLUGINS=1 to disable plugins.                        |
+| $KUBECONFIG                           | set an alternative Kubernetes configuration file (default "~/.kube/config")       |
++------------------+--------------------------------------------------------------------------------------------------------+
 
 Helm stores configuration based on the XDG base directory specification, so
 
