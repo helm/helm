8b8ca996f3f49344196462b9babaea3434ee1971
Liu Ming <hit_oak_tree@126.com>
2020-04-30 17:31:27 +0800

    polish to keep the same log style
    Signed-off-by: Liu Ming <hit_oak_tree@126.com>
    (cherry picked from commit ff3ed53b7c0c07aab2bc8cc59344d18063e8a719)
    (cherry picked from commit 07255156767ecc6e53c84d6dcd4c2fda79d8a440)

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -429,7 +429,7 @@ func updateResource(c *Client, target *resource.Info, currentObj runtime.Object,
 		if err != nil {
 			return errors.Wrap(err, "failed to replace object")
 		}
-		c.Log("Replaced %q with kind %s for kind %s\n", target.Name, currentObj.GetObjectKind().GroupVersionKind().Kind, kind)
+		c.Log("Replaced %q with kind %s for kind %s", target.Name, currentObj.GetObjectKind().GroupVersionKind().Kind, kind)
 	} else {
 		patch, patchType, err := createPatch(target, currentObj)
 		if err != nil {
