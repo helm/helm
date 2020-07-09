a3541274baecb2fdff7da8893689588d1bf397ab
Matt Butcher <matt.butcher@microsoft.com>
2020-07-09 14:31:51 -0600

    Lint dependencies (#7970)
    * feat: add dependency tests
    
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>
    
    * replaced on-disk fixtures with in-memory fixtures
    
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- pkg/lint/lint_test.go
+++ pkg/lint/lint_test.go
@@ -38,12 +38,12 @@ const goodChartDir = "rules/testdata/goodone"
 
 func TestBadChart(t *testing.T) {
 	m := All(badChartDir, values, namespace, strict).Messages
-	if len(m) != 7 {
+	if len(m) != 8 {
 		t.Errorf("Number of errors %v", len(m))
 		t.Errorf("All didn't fail with expected errors, got %#v", m)
 	}
-	// There should be one INFO, 2 WARNINGs and one ERROR messages, check for them
-	var i, w, e, e2, e3, e4, e5 bool
+	// There should be one INFO, 2 WARNINGs and 2 ERROR messages, check for them
+	var i, w, e, e2, e3, e4, e5, e6 bool
 	for _, msg := range m {
 		if msg.Severity == support.InfoSev {
 			if strings.Contains(msg.Err.Error(), "icon is recommended") {
@@ -74,9 +74,13 @@ func TestBadChart(t *testing.T) {
 			if strings.Contains(msg.Err.Error(), "dependencies are not valid in the Chart file with apiVersion") {
 				e5 = true
 			}
+			// This comes from the dependency check, which loads dependency info from the Chart.yaml
+			if strings.Contains(msg.Err.Error(), "unable to load chart") {
+				e6 = true
+			}
 		}
 	}
-	if !e || !e2 || !e3 || !e4 || !e5 || !w || !i {
+	if !e || !e2 || !e3 || !e4 || !e5 || !w || !i || !e6 {
 		t.Errorf("Didn't find all the expected errors, got %#v", m)
 	}
 }
