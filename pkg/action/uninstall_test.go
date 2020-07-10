9f7b5d29e6a470470a3c4705196f368b623f7116
Dong Gang <dong.gang@daocloud.io>
2020-07-10 10:48:00 +0800

    Fix the style error
    Signed-off-by: Dong Gang <dong.gang@daocloud.io>

--- pkg/action/uninstall_test.go
+++ pkg/action/uninstall_test.go
@@ -9,11 +9,11 @@ import (
 func TestUninstall_deleteRelease(t *testing.T) {
 	is := assert.New(t)
 	rel := releaseStub()
-	rel.Chart =buildChart(withKeepAnnoManifestTemplate())
+	rel.Chart = buildChart(withKeepAnnoManifestTemplate())
 	rel.Manifest = manifestWithKeepAnno
 	config := actionConfigFixture(t)
 	unisAction := NewUninstall(config)
 	str, errs := unisAction.deleteRelease(rel)
-	is.Len(errs,0)
-	is.Equal("pod/pod-keep\n",str)
+	is.Len(errs, 0)
+	is.Equal("pod/pod-keep\n", str)
 }
