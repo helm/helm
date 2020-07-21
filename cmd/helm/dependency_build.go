3e275baef7c96728a2ec7df27f5bbcae89a1c0bb
Dong Gang <dong.gang@daocloud.io>
2020-07-21 10:11:19 +0800

    fix(sdk): Polish the downloader/manager package error return
    Close #8471
    
    Signed-off-by: Dong Gang <dong.gang@daocloud.io>

--- cmd/helm/dependency_build.go
+++ cmd/helm/dependency_build.go
@@ -19,7 +19,9 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"k8s.io/client-go/util/homedir"
 
@@ -65,7 +67,11 @@ func newDependencyBuildCmd(out io.Writer) *cobra.Command {
 			if client.Verify {
 				man.Verify = downloader.VerifyIfPossible
 			}
-			return man.Build()
+			err := man.Build()
+			if e, ok := err.(downloader.ErrRepoNotFound); ok {
+				return errors.Errorf("no repository definition for %s. Please add the missing repos via 'helm repo add'", strings.Join(e.Repos, ", "))
+			}
+			return err
 		},
 	}
 
