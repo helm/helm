36a7214faab4352ec76ff6bc091b17f7b270d655
Boris Dudelsack <bdudelsack@gmail.com>
2020-08-24 17:04:16 +0200

    Test the version number for semver compatibility
    Signed-off-by: Boris Dudelsack <bdudelsack@gmail.com>

---
+++ internal/version/version_test.go
@@ -0,0 +1,32 @@
+/*
+Copyright The Helm Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package version
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Masterminds/semver/v3"
+)
+
+func TestVersionSemver(t *testing.T) {
+	_, err := semver.StrictNewVersion(strings.TrimPrefix(version, "v"))
+
+	if err != nil {
+		t.Fatalf("The version number %s is not semver compliant", version)
+	}
+}
