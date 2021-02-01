64e2d596cf17688d4db1446c62255b07c755db64
Jack Whitter-Jones <jackwhitterjones@gmail.com>
2021-02-01 10:12:35 +0000

    Fix-9253: Change the deprecated charts repo URL in release notes
    What this PR does / why we need it:
    fix for issue #9253. The link taken from the projects GitHub page has been used for consistency across the documentation.
    
    Signed-off-by: Jack Whitter-Jones <jackwhitterjones@gmail.com>

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -70,7 +70,7 @@ The community keeps growing, and we'd love to see you there!
   - `#helm-users` for questions and just to hang out
   - `#helm-dev` for discussing PRs, code, and bugs
 - Hang out at the Public Developer Call: Thursday, 9:30 Pacific via [Zoom](https://zoom.us/j/696660622)
-- Test, debug, and contribute charts: [GitHub/helm/charts](https://github.com/helm/charts)
+- Test, debug, and contribute charts: [ArtifactHub/packages](https://artifacthub.io/packages/search?kind=0)
 
 ## Notable Changes
 
