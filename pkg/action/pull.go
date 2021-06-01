b1a5d7c04c21fa323c1feffda02b8ad132e59a70
Andreas Karis <ak.karis@gmail.com>
2021-06-01 14:58:31 +0200

    fix(helm): helm pull --repo to use repo creds from helm repos file
    Up to this point, helm pull --repo required --username and --password
    even if the repository had already been added via helm repo add.
    
    From now on, check repositories in the repositories file and if the URL
    matches, pull the chart with the username and password from the entry.
    
    Fixes #9599
    
    Co-authored-by: Andreas Karis <ak.karis@gmail.com>
    Co-authored-by: Felipe Santos <felipecassiors@gmail.com>
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- pkg/action/pull.go
+++ pkg/action/pull.go
@@ -18,6 +18,7 @@ package action
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"strings"
@@ -73,6 +74,23 @@ func (p *Pull) SetRegistryClient(client *registry.Client) {
 func (p *Pull) Run(chartRef string) (string, error) {
 	var out strings.Builder
 
+	// If the repository URL is set via CLI (--repo), but neither a username nor
+	// password are set for a pull request, check if the repository credentials
+	// are already stored in helm's repository config.
+	if p.RepoURL != "" {
+		if p.Username == "" && p.Password == "" {
+			f, err := repo.LoadFile(p.Settings.RepositoryConfig)
+			if err != nil {
+				return out.String(), err
+			}
+			if repoEntry := f.GetByURL(p.RepoURL); repoEntry != nil {
+				slog.Info("Using credentials from repository config")
+				p.Username = repoEntry.Username
+				p.Password = repoEntry.Password
+			}
+		}
+	}
+
 	c := downloader.ChartDownloader{
 		Out:     &out,
 		Keyring: p.Keyring,
@@ -113,7 +131,6 @@ func (p *Pull) Run(chartRef string) (string, error) {
 		}
 		defer os.RemoveAll(dest)
 	}
-
 	if p.RepoURL != "" {
 		chartURL, err := repo.FindChartInRepoURL(
 			p.RepoURL,
