13d86143f8f1b8aac1e0df9410c3699868109af6
David Xia <dxia@spotify.com>
2021-12-02 16:03:03 -0500

    docs: Clarify use of --set-file
    and fix non-idiomatic English.
    
    replaces https://github.com/helm/helm-www/pull/1239
    
    Signed-off-by: David Xia <dxia@spotify.com>

--- cmd/helm/upgrade.go
+++ cmd/helm/upgrade.go
@@ -49,9 +49,9 @@ version will be specified unless the '--version' flag is set.
 
 To override values in a chart, use either the '--values' flag and pass in a file
 or use the '--set' flag and pass configuration from the command line, to force string
-values, use '--set-string'. In case a value is large and therefore
-you want not to use neither '--values' nor '--set', use '--set-file' to read the
-single large value from file.
+values, use '--set-string'. You can use '--set-file' to set individual
+values from a file when the value itself is too long for the command line
+or is dynamically generated.
 
 You can specify the '--values'/'-f' flag multiple times. The priority will be given to the
 last (right-most) file specified. For example, if both myvalues.yaml and override.yaml
