43764cea41c3c9c2c1f7430c24a8e6e024fb79f3
Josh Dolitsky <josh@dolit.ski>
2022-04-27 12:24:51 -0500

    move pusher stuff out of experimental
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- internal/experimental/action/push.go
+++ internal/experimental/action/push.go
@@ -19,11 +19,11 @@ package action
 import (
 	"strings"
 
-	"helm.sh/helm/v3/internal/experimental/pusher"
 	"helm.sh/helm/v3/internal/experimental/registry"
-	"helm.sh/helm/v3/internal/experimental/uploader"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/cli"
+	"helm.sh/helm/v3/pkg/pusher"
+	"helm.sh/helm/v3/pkg/uploader"
 )
 
 // Push is the action for uploading a chart.
