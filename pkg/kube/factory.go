74fb5790767da05100340e71b137b6de4a6f4772
Andrew Seigner <andrew@sig.gy>
2022-05-04 10:58:09 +0000

    Bump k8s packages from 0.23.6 to 0.24.0
    This required modifying the `kube.Factory` interface to conform to
    changes in k8s' `cmdutil.Factory` interface:
    https://github.com/kubernetes/kubernetes/commit/fe3772890f650f9bcf020b43dc5a51fab0fa17f4
    
    Signed-off-by: Andrew Seigner <andrew@sig.gy>
    Signed-off-by: Gong Yongjie <yong-jie.gong@microfocus.com>

--- pkg/kube/factory.go
+++ pkg/kube/factory.go
@@ -18,6 +18,8 @@ package kube // import "helm.sh/helm/v3/pkg/kube"
 
 import (
 	"k8s.io/cli-runtime/pkg/resource"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/kubectl/pkg/validation"
@@ -28,11 +30,19 @@ import (
 type Factory interface {
 	// ToRawKubeConfigLoader return kubeconfig loader as-is
 	ToRawKubeConfigLoader() clientcmd.ClientConfig
+
+	// DynamicClient returns a dynamic client ready for use
+	DynamicClient() (dynamic.Interface, error)
+
 	// KubernetesClientSet gives you back an external clientset
 	KubernetesClientSet() (*kubernetes.Clientset, error)
+
 	// NewBuilder returns an object that assists in loading objects from both disk and the server
 	// and which implements the common patterns for CLI interactions with generic resources.
 	NewBuilder() *resource.Builder
+
 	// Returns a schema that can validate objects stored on disk.
-	Validator(validate bool) (validation.Schema, error)
+	Validator(validationDirective string, verifier *resource.QueryParamVerifier) (validation.Schema, error)
+	// OpenAPIGetter returns a getter for the openapi schema document
+	OpenAPIGetter() discovery.OpenAPISchemaInterface
 }
