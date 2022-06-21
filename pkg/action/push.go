b0ecb210563ccf0441020e8658c7f7e492c6fb0a
Soule BA <bah.soule@gmail.com>
2022-06-21 13:30:41 +0200

    Enable custom certificates option for OCI
    If implemented, users will be able to use custom certificates and CA to
    while interacting with OCI registries.
    
    Signed-off-by: Soule BA <bah.soule@gmail.com>

--- pkg/action/push.go
+++ pkg/action/push.go
@@ -17,6 +17,7 @@ limitations under the License.
 package action
 
 import (
+	"io"
 	"strings"
 
 	"helm.sh/helm/v3/pkg/cli"
@@ -31,6 +32,10 @@ import (
 type Push struct {
 	Settings *cli.EnvSettings
 	cfg      *Configuration
+	certFile string
+	keyFile  string
+	caFile   string
+	out      io.Writer
 }
 
 // PushOpt is a type of function that sets options for a push action.
@@ -40,6 +45,22 @@ type PushOpt func(*Push)
 func WithPushConfig(cfg *Configuration) PushOpt {
 	return func(p *Push) {
 		p.cfg = cfg
+	}
+}
+
+// WithTLSClientConfig sets the certFile, keyFile, and caFile fields on the push configuration object.
+func WithTLSClientConfig(certFile, keyFile, caFile string) PushOpt {
+	return func(p *Push) {
+		p.certFile = certFile
+		p.keyFile = keyFile
+		p.caFile = caFile
+	}
+}
+
+// WithOptWriter sets the registryOut field on the push configuration object.
+func WithPushOptWriter(out io.Writer) PushOpt {
+	return func(p *Push) {
+		p.out = out
 	}
 }
 
@@ -59,10 +80,24 @@ func (p *Push) Run(chartRef string, remote string) (string, error) {
 	c := uploader.ChartUploader{
 		Out:     &out,
 		Pushers: pusher.All(p.Settings),
-		Options: []pusher.Option{},
+		Options: []pusher.Option{
+			pusher.WithTLSClientConfig(p.certFile, p.keyFile, p.caFile),
+		},
 	}
 
 	if registry.IsOCI(remote) {
+		// Provide a tls enabled client for the pull command if the user has
+		// specified the cert file or key file or ca file.
+		if (p.certFile != "" && p.keyFile != "") || p.caFile != "" {
+			registryClient, err := registry.NewRegistryClientWithTLS(p.out, p.certFile, p.keyFile, p.caFile,
+				p.Settings.RegistryConfig, p.Settings.Debug)
+			if err != nil {
+				return out.String(), err
+			}
+			p.cfg.RegistryClient = registryClient
+		}
+
+		// Don't use the default registry client if tls options are set.
 		c.Options = append(c.Options, pusher.WithRegistryClient(p.cfg.RegistryClient))
 	}
 
