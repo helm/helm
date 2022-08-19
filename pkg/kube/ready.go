9481cf16c454cb83d0752a1f279e6bbec0fb68e6
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2022-08-19 21:18:52 +1000

    Use own scheme rather than mutating a global shared one
    Signed-off-by: Mikhail Mazurskiy <mmazurskiy@gitlab.com>

--- pkg/kube/ready.go
+++ pkg/kube/ready.go
@@ -30,7 +30,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/cli-runtime/pkg/resource"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
 
 	deploymentutil "helm.sh/helm/v4/internal/third_party/k8s.io/kubernetes/deployment/util"
 )
@@ -148,7 +147,7 @@ func (c *ReadyChecker) IsReady(ctx context.Context, v *resource.Info) (bool, err
 			return false, err
 		}
 		crd := &apiextv1beta1.CustomResourceDefinition{}
-		if err := scheme.Scheme.Convert(v.Object, crd, nil); err != nil {
+		if err := NativeScheme().Convert(v.Object, crd, nil); err != nil {
 			return false, err
 		}
 		if !c.crdBetaReady(*crd) {
@@ -159,7 +158,7 @@ func (c *ReadyChecker) IsReady(ctx context.Context, v *resource.Info) (bool, err
 			return false, err
 		}
 		crd := &apiextv1.CustomResourceDefinition{}
-		if err := scheme.Scheme.Convert(v.Object, crd, nil); err != nil {
+		if err := NativeScheme().Convert(v.Object, crd, nil); err != nil {
 			return false, err
 		}
 		if !c.crdReady(*crd) {
