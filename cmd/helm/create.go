4fd366296ac2bdbbe1e3059c13befe9a677f14c8
Filip Krakowski <krakowski@hhu.de>
2022-09-09 13:40:14 +0200

    Fix go linting errors
    Signed-off-by: Filip Krakowski <krakowski@hhu.de>

--- cmd/helm/create.go
+++ cmd/helm/create.go
@@ -23,9 +23,9 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"helm.sh/helm/v3/pkg/chart"
 
 	"helm.sh/helm/v3/cmd/helm/require"
+	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/chartutil"
 	"helm.sh/helm/v3/pkg/helmpath"
 )
