290397beb8b0fcb4f5396a2b5f813a4f362edbc0
Mikhail Kopylov <mih.kopylov@yandex.ru>
2023-01-07 16:41:30 +0300

    Adjust `get` command description to account metadata
    Signed-off-by: Mikhail Kopylov <mih.kopylov@yandex.ru>

--- cmd/helm/get.go
+++ cmd/helm/get.go
@@ -33,6 +33,7 @@ get extended information about the release, including:
 - The generated manifest file
 - The notes provided by the chart of the release
 - The hooks associated with the release
+- The metadata of the release
 `
 
 func newGetCmd(cfg *action.Configuration, out io.Writer) *cobra.Command {
