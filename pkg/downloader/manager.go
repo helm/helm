767eb92f5b92697f71e356310cc941d8205bf916
Vaibhav Sharma <17532va@gmail.com>
2023-01-20 13:01:39 +0530

    Moved the logging after error
    Signed-off-by: Vaibhav Sharma <17532va@gmail.com>

--- pkg/downloader/manager.go
+++ pkg/downloader/manager.go
@@ -85,14 +85,14 @@ type Manager struct {
 // If SkipUpdate is set, this will not update the repository.
 func (m *Manager) Build() error {
 	c, err := m.loadChartDir()
-	if c.Metadata.Dependencies == nil && m.Debug {
-		fmt.Print("No dependencies found to build")
-		return nil
-	}
 	if err != nil {
 		return err
 	}
 
+	if c.Metadata.Dependencies == nil && m.Debug {
+		fmt.Print("No dependencies found to build")
+		return nil
+	}
 	// If a lock file is found, run a build from that. Otherwise, just do
 	// an update.
 	lock := c.Lock
