2b324f1735d71c71fd363c1b232840d5bc99e726
Matt Farina <matt.farina@suse.com>
2023-03-09 13:27:22 -0500

    Bump the Go version
    Needed to gofmt source to meet changes in style
    
    Signed-off-by: Matt Farina <matt.farina@suse.com>
    Signed-off-by: igorushi <igor.altman@gmail.com>

--- pkg/chartutil/coalesce.go
+++ pkg/chartutil/coalesce.go
@@ -37,11 +37,11 @@ func concatPrefix(a, b string) string {
 //
 // Values are coalesced together using the following rules:
 //
-//	- Values in a higher level chart always override values in a lower-level
-//		dependency chart
-//	- Scalar values and arrays are replaced, maps are merged
-//	- A chart has access to all of the variables for it, as well as all of
-//		the values destined for its dependencies.
+//   - Values in a higher level chart always override values in a lower-level
+//     dependency chart
+//   - Scalar values and arrays are replaced, maps are merged
+//   - A chart has access to all of the variables for it, as well as all of
+//     the values destined for its dependencies.
 func CoalesceValues(chrt *chart.Chart, vals map[string]interface{}) (Values, error) {
 	v, err := copystructure.Copy(vals)
 	if err != nil {
