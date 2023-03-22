2f13355e40ca2790bebb19b7182500d312a536de
AdamKorcz <adam@adalogics.com>
2023-03-22 14:56:41 +0000

    pkg/chartutil: fix nil-dereference
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- pkg/chartutil/dependencies.go
+++ pkg/chartutil/dependencies.go
@@ -137,6 +137,9 @@ Loop:
 	}
 
 	for _, req := range c.Metadata.Dependencies {
+		if req == nil {
+			continue
+		}
 		if chartDependency := getAliasDependency(c.Dependencies(), req); chartDependency != nil {
 			chartDependencies = append(chartDependencies, chartDependency)
 		}
