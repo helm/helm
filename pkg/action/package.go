847369c184d93fc4d36e9ec86a388b60331ab37a
Matt Farina <matt.farina@suse.com>
2024-01-05 16:35:24 -0500

    Update to Go 1.21 for builds
    Noteis:
    1. This moves golangci scanning to a GitHub action. This will
       enable inline pointers to issues in the PR where linting fails.
    2. Go 1.21 is specified in the go.mod because Kubernetes libs
       require it.
    3. The lint issues were removed. Some were fixed while others
       were handled by skipping linting or using _ as an argument.
       Many of these can be refactored later for better cleanup.
    
    Signed-off-by: Matt Farina <matt.farina@suse.com>

--- pkg/action/package.go
+++ pkg/action/package.go
@@ -54,7 +54,7 @@ func NewPackage() *Package {
 }
 
 // Run executes 'helm package' against the given chart and returns the path to the packaged chart.
-func (p *Package) Run(path string, vals map[string]interface{}) (string, error) {
+func (p *Package) Run(path string, _ map[string]interface{}) (string, error) {
 	ch, err := loader.LoadDir(path)
 	if err != nil {
 		return "", err
