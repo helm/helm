cf7eb83155f4ffcf88cf63d475dc757c8d1aa091
Danilo Patrucco <danilo.patrucco@gmail.com>
2024-07-05 11:26:53 -0400

    fix import missing
    Signed-off-by: Danilo Patrucco <danilo.patrucco@gmail.com>

--- pkg/lint/rules/isignored.go
+++ pkg/lint/rules/isignored.go
@@ -3,6 +3,7 @@ package rules
 import (
     "path/filepath"
     "strings"
+    "fmt"
 )
 
 func IsIgnored(path string, patterns []string) bool {
@@ -10,6 +11,7 @@ func IsIgnored(path string, patterns []string) bool {
         cleanedPath := filepath.Clean(path)
         cleanedPattern := filepath.Clean(pattern)
         if match, err := filepath.Match(cleanedPattern, cleanedPath); err == nil && match {
+            fmt.Printf("Ignoring path: %s due to pattern: %s\n", path, pattern)
             return true
         }
         if strings.HasSuffix(cleanedPattern, "/") || strings.HasSuffix(cleanedPattern, "\\") {
