951178a4a4ae26d6ba4e6b9ccaa3d212efc0c5ff
Daniel J. Pritchett <dpritchett@radiusmethod.com>
2024-07-23 13:57:51 -0500

    go fmt ignore.go

--- pkg/lint/rules/ignore.go
+++ pkg/lint/rules/ignore.go
@@ -1,33 +1,33 @@
 package rules
 
 import (
-    "bufio"
-    "os"
-    "strings"
+	"bufio"
+	"os"
+	"strings"
 )
 
 func ParseIgnoreFile(filePath string) (map[string][]string, error) {
-    patterns := make(map[string][]string)
-    file, err := os.Open(filePath)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	patterns := make(map[string][]string)
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        line := strings.TrimSpace(scanner.Text())
-        if line != "" && !strings.HasPrefix(line, "#") {
-            parts := strings.SplitN(line, " ", 2)
-            if len(parts) > 1 {
-                // Check if the key already exists and append to its slice
-                patterns[parts[0]] = append(patterns[parts[0]], parts[1])
-            } else if len(parts) == 1 {
-                // Add an empty pattern if only the path is present
-                patterns[parts[0]] = append(patterns[parts[0]], "")
-            }
-        }
-    }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" && !strings.HasPrefix(line, "#") {
+			parts := strings.SplitN(line, " ", 2)
+			if len(parts) > 1 {
+				// Check if the key already exists and append to its slice
+				patterns[parts[0]] = append(patterns[parts[0]], parts[1])
+			} else if len(parts) == 1 {
+				// Add an empty pattern if only the path is present
+				patterns[parts[0]] = append(patterns[parts[0]], "")
+			}
+		}
+	}
 
-    return patterns, scanner.Err()
+	return patterns, scanner.Err()
 }
