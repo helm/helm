42e8af1545fd3ef840ac15d58ccb1a5e0a4c1b88
Vineet Aggarwal <vineet.aggarwal@rakuten.com>
2024-12-02 09:32:17 +0530

    removed copyright notice from the PR
    Signed-off-by: Vineet Aggarwal <vineet.aggarwal@rakuten.com>

--- pkg/cli/values/options_test.go
+++ pkg/cli/values/options_test.go
@@ -1,6 +1,5 @@
 /*
 Copyright The Helm Authors.
-Copyright (c) 2024 Rakuten Symphony India.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
