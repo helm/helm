2cf04bed2e510feaa306e9470dcbd771966d909e
George Jenkins <gvjenkins@gmail.com>
2025-01-05 08:32:02 -0800

    cleanup moved errors
    Signed-off-by: George Jenkins <gvjenkins@gmail.com>

--- pkg/chartutil/errors.go
+++ pkg/chartutil/errors.go
@@ -20,20 +20,6 @@ import (
 	"fmt"
 )
 
-// ErrNoTable indicates that a chart does not have a matching table.
-type ErrNoTable struct {
-	Key string
-}
-
-func (e ErrNoTable) Error() string { return fmt.Sprintf("%q is not a table", e.Key) }
-
-// ErrNoValue indicates that Values does not contain a key with a value
-type ErrNoValue struct {
-	Key string
-}
-
-func (e ErrNoValue) Error() string { return fmt.Sprintf("%q is not a value", e.Key) }
-
 type ErrInvalidChartName struct {
 	Name string
 }
