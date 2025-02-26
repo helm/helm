d6d17a76c4ff0a497d19f0d1e030933ad0dfdfc0
Matt Farina <matt.farina@suse.com>
2025-02-26 16:01:11 -0500

    Updating checkIfInstallable to handle multiple chart apiVersions
    Signed-off-by: Matt Farina <matt.farina@suse.com>

---
+++ internal/chart/chart.go
@@ -0,0 +1,29 @@
+/*
+Copyright The Helm Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package chart
+
+import (
+	chartv2 "helm.sh/helm/v4/pkg/chart/v2"
+)
+
+// Chart provides an interface between multiple types of charts.
+//
+// Chat provides a union between different Chart versions so that functions and generics can work
+// with one type. Note, chart v2 handles apiVersion v1 and v2 charts.
+// TODO(mattfarina): Add chart v3 here.
+type Chart interface {
+	chartv2.Chart
+}
