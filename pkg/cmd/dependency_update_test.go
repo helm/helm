ac3950670c9abcd05d582015cd88f63285098594
Andrea Tartaglia <me@andreatartaglia.com>
2025-03-03 12:25:03 +0100

    add untar option to dependency update
    Signed-off-by: Andrea Tartaglia <me@andreatartaglia.com>

--- pkg/cmd/dependency_update_test.go
+++ pkg/cmd/dependency_update_test.go
@@ -147,6 +147,19 @@ func TestDependencyUpdateCmd(t *testing.T) {
 	if _, err := os.Stat(expect); err != nil {
 		t.Fatal(err)
 	}
+
+	// When using `--untar`, ./charts/*.tgz should not exist
+	_, _, err = executeActionCommand(
+		fmt.Sprintf("dependency update '%s' --repository-config %s --repository-cache %s --untar", dir(chartname), dir("repositories.yaml"), dir()),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Make sure the actual file is downloaded and untar
+	expect = dir(chartname, "charts/reqtest")
+	if _, err := os.Stat(expect); err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestDependencyUpdateCmd_DoNotDeleteOldChartsOnError(t *testing.T) {
@@ -236,7 +249,6 @@ func TestDependencyUpdateCmd_WithRepoThatWasNotAdded(t *testing.T) {
 		fmt.Sprintf("dependency update '%s' --repository-config %s --repository-cache %s", dir(chartname),
 			dir("repositories.yaml"), dir()),
 	)
-
 	if err != nil {
 		t.Logf("Output: %s", out)
 		t.Fatal(err)
