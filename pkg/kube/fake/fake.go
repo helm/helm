4ee3a19e9a98e3b58eb4579f019666e834408ca3
Patrick Seidensal <pseidensal@suse.com>
2025-03-21 11:48:32 +0100

    Fix --take-ownership
    If a resource exists in the cluster and is to be adopted by helm install
    --take-ownership, it is left unchanged while helm reports the
    installation to have succeeded.
    
    This is due to CRs and CRDs being merged without three-way-merge, which
    results in an empty patch.
    
    By using a three-way-merge transparently when --take-ownership is used,
    the helm behaves as expected without breaking previous behavior.
    
    Fixes #30622
    
    Signed-off-by: Patrick Seidensal <pseidensal@suse.com>

--- pkg/kube/fake/fake.go
+++ pkg/kube/fake/fake.go
@@ -114,6 +114,14 @@ func (f *FailingKubeClient) Update(r, modified kube.ResourceList, ignoreMe bool)
 	return f.PrintingKubeClient.Update(r, modified, ignoreMe)
 }
 
+// Update returns the configured error if set or prints
+func (f *FailingKubeClient) UpdateThreeWayMerge(r, modified kube.ResourceList, ignoreMe bool) (*kube.Result, error) {
+	if f.UpdateError != nil {
+		return &kube.Result{}, f.UpdateError
+	}
+	return f.PrintingKubeClient.Update(r, modified, ignoreMe)
+}
+
 // Build returns the configured error if set or prints
 func (f *FailingKubeClient) Build(r io.Reader, _ bool) (kube.ResourceList, error) {
 	if f.BuildError != nil {
