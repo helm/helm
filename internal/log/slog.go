947658a96eee63ac1315c3e188775a430c0af6d5
Benoit Tigeot <benoit.tigeot@lifen.fr>
2025-03-31 14:39:53 +0200

    Explain why we ignore the timestamp
    Signed-off-by: Benoit Tigeot <benoit.tigeot@lifen.fr>

--- internal/log/slog.go
+++ internal/log/slog.go
@@ -59,6 +59,7 @@ func NewReadableTextLogger(output io.Writer, debugEnabled bool) Logger {
 
 	handler := slog.NewTextHandler(output, &slog.HandlerOptions{
 		Level: level,
+		// Ignore the time key to avoid cluttering the output with timestamps
 		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
 			if a.Key == slog.TimeKey {
 				return slog.Attr{}
