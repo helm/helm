b622eafa4cf6781db27853336f17ceb0a4c0d755
Benoit Tigeot <benoit.tigeot@lifen.fr>
2025-04-16 17:02:32 +0200

    No need to setup max size for loader for package command
    Signed-off-by: Benoit Tigeot <benoit.tigeot@lifen.fr>

--- pkg/action/package.go
+++ pkg/action/package.go
@@ -53,10 +53,6 @@ type Package struct {
 	KeyFile               string
 	CaFile                string
 	InsecureSkipTLSverify bool
-	// MaxChartSize is the maximum size of a decompressed chart in bytes
-	MaxChartSize int64
-	// MaxFileSize is the maximum size of a single file in a chart in bytes
-	MaxFileSize int64
 }
 
 // NewPackage creates a new Package object with the given configuration.
