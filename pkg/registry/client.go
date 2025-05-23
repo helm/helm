937c533e37f3a636edd217d956af62dec38995fc
Robert Sirchia <rsirchia@outlook.com>
2025-05-23 16:19:03 -0400

    forward porting 30902
    Signed-off-by: Robert Sirchia <rsirchia@outlook.com>

--- pkg/registry/client.go
+++ pkg/registry/client.go
@@ -463,7 +463,7 @@ func (c *Client) Pull(ref string, options ...PullOption) (*PullResult, error) {
 			PreCopy: func(_ context.Context, desc ocispec.Descriptor) error {
 				mediaType := desc.MediaType
 				if i := sort.SearchStrings(allowedMediaTypes, mediaType); i >= len(allowedMediaTypes) || allowedMediaTypes[i] != mediaType {
-					return fmt.Errorf("media type %q is not allowed, found in descriptor with digest: %q", mediaType, desc.Digest)
+					return oras.SkipNode
 				}
 
 				mu.Lock()
