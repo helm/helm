0d6de28bf446b4d1b2cb71e0441e5c8ebc9e91c9
tison <wander4096@gmail.com>
2025-10-24 15:02:25 +0800

    chore: fix typo of public field
    Signed-off-by: tison <wander4096@gmail.com>

--- pkg/action/package.go
+++ pkg/action/package.go
@@ -57,7 +57,7 @@ type Package struct {
 	CertFile              string
 	KeyFile               string
 	CaFile                string
-	InsecureSkipTLSverify bool
+	InsecureSkipTLSVerify bool
 }
 
 const (
