60294548e05945975abe35aaa382a99674690ff2
Benoit Tigeot <benoit.tigeot@lifen.fr>
2026-01-21 09:47:28 +0100

    refactor: remove per-file decompression size limit
    Remove MaxDecompressedFileSize as it's no longer necessary after
    migrating to a maintained JSON schema library (santhosh-tekuri/jsonschema/v6).
    The original limit was added to protect against vulnerabilities in an
    unmaintained library.
    
    The total decompressed chart size limit (MaxDecompressedChartSize) remains
    to protect against other attack vectors.
    
    Partially resolves #30738
    
    Related:
      - https://github.com/helm/helm/pull/30743
    
    Signed-off-by: Benoit Tigeot <benoit.tigeot@lifen.fr>

--- pkg/chart/loader/archive/archive.go
+++ pkg/chart/loader/archive/archive.go
@@ -37,10 +37,6 @@ import (
 // The default value is 100 MiB.
 var MaxDecompressedChartSize int64 = 100 * 1024 * 1024 // Default 100 MiB
 
-// MaxDecompressedFileSize is the size of the largest file that Helm will attempt to load.
-// The size of the file is the decompressed version of it when it is stored in an archive.
-var MaxDecompressedFileSize int64 = 5 * 1024 * 1024 // Default 5 MiB
-
 var drivePathPattern = regexp.MustCompile(`^[a-zA-Z]:/`)
 
 var utf8bom = []byte{0xEF, 0xBB, 0xBF}
@@ -126,10 +122,6 @@ func LoadArchiveFiles(in io.Reader) ([]*BufferedFile, error) {
 
 		if hd.Size > remainingSize {
 			return nil, fmt.Errorf("decompressed chart is larger than the maximum size %d", MaxDecompressedChartSize)
-		}
-
-		if hd.Size > MaxDecompressedFileSize {
-			return nil, fmt.Errorf("decompressed chart file %q is larger than the maximum file size %d", hd.Name, MaxDecompressedFileSize)
 		}
 
 		limitedReader := io.LimitReader(tr, remainingSize)
